028555a1eaa5c822608268417bbd535ac76fb0aa
Abhishek Sharma <abhishek859573@gmail.com>
2024-03-03 19:54:04 +0530

    Todo

---
+++ script.js
@@ -0,0 +1,37 @@
+let todoList = [
+  { item: 'buy Milk', dueDate: '4/10/2024' },
+  { item: 'Go to collage', dueDate: '4/10/2024' }
+];
+
+displayItems();
+
+function addTodo() {
+  let inputElement = document.querySelector('#todo-input');
+  let dateElement = document.querySelector('#todo-date');
+  
+  let todoItem = inputElement.value;
+  let todoDate = dateElement.value;
+  todoList.push({item:todoItem, dueDate: todoDate});
+  inputElement.value = '';
+  dateElement.value = '';
+  displayItems();
+}
+
+function displayItems() {
+  let containerElement = document.querySelector('.todo-container');
+  let newHtml = '';
+  for (let i = 0; i < todoList.length; i++){
+    let Item = todoList[i].item;
+    let dueDate = todoList[i].dueDate;
+
+    // let {Item, dueDate} = todoList[i];
+    newHtml += `
+    <span>${Item}</span>
+    <span>${dueDate}</span>
+    <button class="btn-delete" onclick = "todoList.splice(${i}, 1);
+    displayItems();"> Delete </button>
+
+    `
+  }
+  containerElement.innerHTML = newHtml;
+}
